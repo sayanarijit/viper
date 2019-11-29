4fe756a6d44b840fdfc19f32562ba0db58e26e18
Arijit Basu <sayanarijit@gmail.com>
2019-11-29 12:15:52 +0530

    Improve docs

--- viper/demo/__init__.py
+++ viper/demo/__init__.py
@@ -1,23 +1,29 @@
 """## Viper CLI Examples.
 
 
-    ### Load hosts from file
+### Load hosts from file
     viper hosts:from_file tests/data/hosts.csv --indent 4
+    
+    # or with a custom loader
+    
     viper hosts:from_file tests/data/hosts.json --loader viper.demo.loaders.json --indent 4
 
-    ### Load hosts from Python object
+### Load hosts from Python object
     viper hosts:from_obj viper.demo.hosts.group1 --indent 4
 
-    ### Let's save the hosts
+### Let's save the hosts
     hosts=$(viper hosts:from_obj viper.demo.hosts.group1)
 
-    ### Filter hosts
+### Filter hosts
     echo $hosts | viper hosts:filter viper.demo.filters.ip_starts_with_2 --indent 4
 
-    ### Assign tasks to the given hosts
+### Assign tasks to the given hosts
     echo $hosts | viper hosts:task viper.demo.tasks.ping --indent 4
 
-    ### Run assigned tasks
+### Run assigned tasks
     echo $hosts | viper hosts:task viper.demo.tasks.ping | viper runners:run --indent 4
+
+    # or use a shortcut
+
     echo $hosts | viper hosts:run_task viper.demo.tasks.ping --indent 4
 """
